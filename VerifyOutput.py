c0660227995a9ca016f574bf89591b40cd7e8933
PramodJose <pramod.gj17@gmail.com>
2018-09-20 21:43:00 +0530

    Verifies the output sent back to the server

---
+++ VerifyOutput.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+inFileName = sys.argv[1]
+refFileName = "expectedOutput.txt"
+
+inFile = open(inFileName, "r")
+refFile = open(refFileName, "r")
+
+inLine = inFile.readline()
+refLine = refFile.readline()
+testCaseNum = 1
+passedAll = True
+
+while len(refLine) > 5:
+    inDict = json.loads(inLine)
+    refDict = json.loads(refLine)
+
+    if inDict != refDict:
+        if passedAll:
+            print("\nThe program failed for the follolwing test case(s):-")
+        passedAll = False
+        print(testCaseNum)
+
+    testCaseNum += 1
+    inLine = inFile.readline()
+    refLine = refFile.readline()
+
+if passedAll:
+    print("\nThe program passed all the test cases!!")
